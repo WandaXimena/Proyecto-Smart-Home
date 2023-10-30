2d15a9a9a4896ab0007539f8ece4dcb5176a72a1
MrEduCR <145067441+MrEduCR@users.noreply.github.com>
2023-10-29 21:08:36 -0600

    Update SmartHomeTesteo.py

--- SmartHomeTesteo.py
+++ SmartHomeTesteo.py
@@ -1,1 +1,34 @@
+#---Proyecto smart home---
+
+
+username = input("Ingrese su username: ") #Input cuando tengamos capacidad de guardar datos
+usuariosValidos = ["edu123","dereck456","felipe789"] #Lista de constantes por mientras
+
+if username in usuariosValidos:
+    print("Usuario válido")
+    ingresoContraseña = input("Ingresa tu contrasena: ")
+    contraseña = ("SmartHome") #Input cuando tengamos capacidad de guardar datos, contraseña por mientras
+    if contraseña == ingresoContraseña:
+        print("Bienvenido")
+else:
+    print("Usuario inválido")
+
+
+habitaciones = []
+
+def agregarHabitacion():
+        while True:
+
+            habitacion = input("Ingrese una habitación:")
+            habitaciones.append(habitacion)
+
+            opcion = input("Desea agregar habitacion otra vez 1 si 2 no:")
+
+            if opcion != "1":
+                break
+
+
+agregarHabitacion()
+
+print("Las habitaciones son: ", habitaciones)
 
